be74dc7c6f21b48b5bb142338bd9901d4f5df128
Paritosh Tripathi <paritosh.btech@gmail.com>
2025-05-10 16:42:04 +0800

    fixing compiling issues post Dev merge

--- frontend/src/app/components/canvas/canvas.component.ts
+++ frontend/src/app/components/canvas/canvas.component.ts
@@ -1,7 +1,30 @@
-
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ApexChart, ApexNonAxisChartSeries, ApexResponsive } from 'ng-apexcharts';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexLegend
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  labels: string[];
+  legend: ApexLegend;
+  responsive: ApexResponsive[];
+};
 
 @Component({
   selector: 'app-canvas',
@@ -11,11 +34,51 @@ import { ApexChart, ApexNonAxisChartSeries, ApexResponsive } from 'ng-apexcharts
 })
 export class CanvasComponent implements OnInit {
   insights: any[] = [];
+  chartOptions: Partial<ChartOptions> = {};
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get<any>('http://localhost:3000/api/agent/insights?persona=CXO&fileId=demo')
-      .subscribe(res => this.insights = res.insights);
+      .subscribe(res => {
+        this.insights = res.insights;
+        this.insights.forEach(insight => {
+          if (insight.chartType === 'pie' || insight.chartType === 'donut') {
+            insight.chartOptions = {
+              series: insight.values,
+              chart: {
+                type: insight.chartType,
+                height: 350
+              },
+              labels: insight.labels,
+              responsive: [{
+                breakpoint: 480,
+                options: {
+                  chart: {
+                    width: 300
+                  },
+                  legend: {
+                    position: 'bottom'
+                  }
+                }
+              }]
+            };
+          } else if (insight.chartType === 'bar') {
+            insight.chartOptions = {
+              series: [{
+                name: 'Emissions',
+                data: insight.values
+              }],
+              chart: {
+                type: 'bar',
+                height: 350
+              },
+              xaxis: {
+                categories: insight.labels
+              }
+            };
+          }
+        });
+      });
   }
 }
--- frontend/src/app/modules/canvas/canvas.module.ts
+++ frontend/src/app/modules/canvas/canvas.module.ts
@@ -1,12 +1,17 @@
-
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CanvasComponent } from '../../components/canvas/canvas.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [CanvasComponent],
-  imports: [CommonModule, NgApexchartsModule],
-  exports: [CanvasComponent]
+  imports: [
+    CommonModule,
+    NgApexchartsModule,
+    HttpClientModule
+  ],
+  exports: [CanvasComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class CanvasModule {}
+export class CanvasModule { }
